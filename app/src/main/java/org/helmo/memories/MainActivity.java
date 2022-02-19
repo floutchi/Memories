724c2028bb7af9b096aec833b3dfbb8a80049bc1
Florian DETIFFE <f.detiffe@student.helmo.be>
2022-02-19 15:11:30 +0100

    Initial commit

---
+++ app/src/main/java/org/helmo/memories/MainActivity.java
@@ -0,0 +1,23 @@
+package org.helmo.memories;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import org.helmo.memories.fragments.HomeFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Injecter le fragment dans notre boite (fragment_container)
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, new HomeFragment());
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
